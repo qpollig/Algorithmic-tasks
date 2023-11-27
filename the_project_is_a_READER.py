30c90f0edfcc975bd1d211ed79ed08aba53edd3d
Polina Nemechkina <qpollig@gmail.com>
2023-11-27 22:20:10 +0500

    task complete

--- the_project_is_a_READER.py
+++ the_project_is_a_READER.py
@@ -1,24 +1,45 @@
-import sys
+'''import sys
 
 
 def reader(number_of_applicants, number_of_clock_cycles):
     arr_of_applicants = []
     for i in range(1, number_of_applicants + 1):
         arr_of_applicants.append(i)
-    remainder = number_of_clock_cycles % len(arr_of_applicants)
     print('arr_of_applicants', arr_of_applicants)
-    while len(arr_of_applicants) != 1:
-        if remainder == 0:
-            arr_of_applicants.pop(-1)
-            print('if remainder == 0', arr_of_applicants)
-        else:
-            arr_of_applicants.pop(remainder-1)
-            print('else', arr_of_applicants)
-    return arr_of_applicants
+    if number_of_clock_cycles > number_of_applicants:
+        while len(arr_of_applicants) != 1:
+            remainder = number_of_clock_cycles % len(arr_of_applicants)
+            print('remainder', remainder)
+            if remainder == 0:
+                arr_of_applicants.pop(-1)
+                print('if remainder == 0', arr_of_applicants)
+            else:
+                arr_of_applicants.pop(remainder-1)
+                print('else', arr_of_applicants)
+        return arr_of_applicants
+    else:
+        while len(arr_of_applicants) != 1:
+            arr_of_applicants.pop(number_of_clock_cycles - 1)
+            print(arr_of_applicants)
 
 
 if __name__ == '__main__':
-    clock_cycles = int(sys.stdin.readline().rstrip())
     applicants = int(sys.stdin.readline().rstrip())
+    clock_cycles = int(sys.stdin.readline().rstrip())
     reader_result = reader(applicants, clock_cycles)
-    print(*reader_result)
+    print(reader_result)
+'''
+
+
+def casting_algorithm(N, K):
+    candidates = list(range(1, N + 1))
+    pointer = 0
+    while len(candidates) > 1:
+        pointer = (pointer + K - 1) % len(candidates)
+        candidates.pop(pointer)
+    return candidates[0]
+
+N = int(input())
+K = int(input())
+result = casting_algorithm(N, K)
+print(result)
