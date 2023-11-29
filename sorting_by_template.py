c30248f28d203493190a9f32b5c9bd45663f448e
Polina Nemechkina <qpollig@gmail.com>
2023-11-29 15:30:52 +0500

    the decision has reached an impasse again

--- sorting_by_template.py
+++ sorting_by_template.py
@@ -3,20 +3,31 @@ import sys
 
 def sorting(n, arr, m, template):
     arr_duplicate = []
+    arr_template = []
     for i in arr:
         if i in template:
-            for j in range(1, len(template)):
-                current = i
-                prev = j - 1
-                while prev >= 0 and template[prev] != current:
-                    template[prev + 1] = template[prev]
-                    prev -= 1
-                template[prev + 1] = current
+            arr_template.append(i)
         else:
             arr_duplicate.append(i)
-            arr_duplicate.sort()
-    template.extend(arr_duplicate)
-    return template
+    arr_template = template + arr_template
+    for i in range(1, len(arr_template)):
+        # Сохраняем текущий элемент в переменную current.
+        current = arr_template[i]
+        # Сохраняем индекс предыдущего элемента
+        # в переменную prev (от previous - предыдущий).
+        prev = i - 1
+        # Сравниваем current с предыдущим элементом
+        # и двигаем предыдущий элемент на одну позицию вправо,
+        # пока он больше current и не достигнуто начало массива.
+        while prev >= 0 and arr_template[prev] != current:
+            arr_template[prev + 1] = arr_template[prev]
+            prev -= 1
+        # Вставляем current в отсортированную часть массива на нужное место.
+        arr_template[prev + 1] = current
+    print(arr_template.reverse())
+    arr_duplicate.sort()
+    arr_template.extend(arr_duplicate)
+    return arr_template
 
 
 if __name__ == '__main__':
