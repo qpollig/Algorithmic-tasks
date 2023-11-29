3b3585c0e923298e793adcbad4a76702f55fc0fd
Polina Nemechkina <qpollig@gmail.com>
2023-11-29 15:46:41 +0500

    sorting_by_template complete dict

--- sorting_by_template.py
+++ sorting_by_template.py
@@ -1,33 +1,23 @@
 import sys
 
 
-def sorting(n, arr, m, template):
-    arr_duplicate = []
-    arr_template = []
-    for i in arr:
-        if i in template:
-            arr_template.append(i)
-        else:
-            arr_duplicate.append(i)
-    arr_template = template + arr_template
-    for i in range(1, len(arr_template)):
-        # Сохраняем текущий элемент в переменную current.
-        current = arr_template[i]
-        # Сохраняем индекс предыдущего элемента
-        # в переменную prev (от previous - предыдущий).
-        prev = i - 1
-        # Сравниваем current с предыдущим элементом
-        # и двигаем предыдущий элемент на одну позицию вправо,
-        # пока он больше current и не достигнуто начало массива.
-        while prev >= 0 and arr_template[prev] != current:
-            arr_template[prev + 1] = arr_template[prev]
-            prev -= 1
-        # Вставляем current в отсортированную часть массива на нужное место.
-        arr_template[prev + 1] = current
-    print(arr_template.reverse())
-    arr_duplicate.sort()
-    arr_template.extend(arr_duplicate)
-    return arr_template
+def sort_by_template(arr, template):
+    # создаем словарь для подсчета количества чисел
+    count = {}
+    for num in arr:
+        count[num] = count.get(num, 0) + 1
+    result = []
+    for num in template:
+        # добавляем повторяющиеся числа в соответствии с шаблоном
+        result.extend([num] * count[num])
+        # удаляем числа из словаря, чтобы не учитывать их повторно
+        del count[num]
+
+    # добавляем оставшиеся числа, которые не были учтены в шаблоне
+    for num, freq in sorted(count.items()):
+        result.extend([num] * freq)
+
+    return result
 
 
 if __name__ == '__main__':
@@ -35,26 +25,5 @@ if __name__ == '__main__':
     arr = [int(i) for i in sys.stdin.readline().rstrip().split()]
     m = int(sys.stdin.readline().rstrip())
     template = [int(i) for i in sys.stdin.readline().rstrip().split()]
-    print(sorting(n, arr, m, template))
-
-
-
-
-
-
-
-
-
-
-
-
-'''    for i in range(1, m):
-        for j in range(len(arr)):
-            current = arr[j]
-            prev = i - 1
-            while prev >= 0 and template[prev] > current:
-                template[prev + 1] = template[prev]
-                prev -= 1
-            template[prev + 1] = current
-            print(f'Шаг {i}, отсортировано элементов: {i + 1}, {arr}')
-'''
+    result = sort_by_template(arr, template)
+    print(*result)
