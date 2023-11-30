3e316c64353e3b433719a2f577f733ddc1c3f6b6
Polina Nemechkina <qpollig@gmail.com>
2023-11-30 19:03:01 +0500

    new_history

--- new_history.py
+++ new_history.py
@@ -1,14 +1,15 @@
 import sys
-from datetime import datetime as dt
 
 
-def yashers(start, end):
-    start_date = dt(int(start[0]), int(start[1]), int(start[2]), int(start[3]), int(start[4]), int(start[5]))
-    end_date = dt(int(end[0]), int(end[1]), int(end[2]), int(end[3]), int(end[4]), int(end[5]))
-    diff_date = end_date - start_date
-    day = diff_date.days
-    sec = diff_date.seconds
-    return day, sec
+def yashers(start_date, end_date):
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    start_year, start_month, start_day, start_hour, start_minute, start_second = map(int, start_date)
+    end_year, end_month, end_day, end_hour, end_minute, end_second = map(int, end_date)
+    diff_years = end_year - start_year - 1
+    total_days = diff_years * sum(days_in_month) + sum(days_in_month[start_month-1:]) + end_day
+    total_seconds = (24*60*60 - (start_hour*60*60 + start_minute*60 + start_second)) + (end_hour*60*60 + end_minute*60 + end_second)
+
+    return total_days, total_seconds
 
 
 if __name__ == '__main__':
