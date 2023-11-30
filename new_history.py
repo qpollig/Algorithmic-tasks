fee64860ac3709897e3c57cbc77300f7dc101b70
Polina Nemechkina <qpollig@gmail.com>
2023-11-30 20:47:25 +0500

    query_to_the_table

--- new_history.py
+++ new_history.py
@@ -5,10 +5,15 @@ def yashers(start_date, end_date):
     days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     start_year, start_month, start_day, start_hour, start_minute, start_second = map(int, start_date)
     end_year, end_month, end_day, end_hour, end_minute, end_second = map(int, end_date)
-    diff_years = end_year - start_year - 1
-    total_days = diff_years * sum(days_in_month) + sum(days_in_month[start_month-1:]) + end_day
-    total_seconds = (24*60*60 - (start_hour*60*60 + start_minute*60 + start_second)) + (end_hour*60*60 + end_minute*60 + end_second)
-
+    days_end = end_year * sum(days_in_month) + sum(days_in_month[0:end_month - 1]) + end_day
+    days_start = start_year * sum(days_in_month) + sum(days_in_month[0:start_month - 1]) + start_day
+    total_days = days_end - days_start
+    end_sec = end_hour * 60 * 60 + end_minute * 60 + end_second
+    start_sec = start_hour * 60 * 60 + start_minute * 60 + start_second
+    total_seconds = end_sec - start_sec
+    if total_seconds < 0:
+        total_days -= 1
+        total_seconds = end_sec + 24 * 60 * 60 - start_sec
     return total_days, total_seconds
 
 
---
+++ query_to_the_table.py
@@ -0,0 +1,46 @@
+import sys
+
+
+def query(N, M, Q):
+    q = 0
+    count = 0
+    count_finaly = 0
+    table = []
+    request_arr = []
+    for i in range(N+1):
+        line_input = input().split()
+        table.append(line_input)
+    for j in range(Q):
+        request = input()
+        request_arr.append(request)
+    for i in range(M):
+        request_now = request_arr[q]
+        colomn_name = request_now[0]
+        operator = request_now[2]
+        number = int(request_now[4])
+        if operator == '<':
+            for j in range(1, N+1):
+                if table[0][i] == colomn_name:
+                    if int(table[j][i]) < number:
+                        count += 1
+            if count == N:
+                count_finaly += N
+        else:
+            for j in range(1, N+1):
+                if table[0][i] == colomn_name:
+                    if int(table[j][i]) > number:
+                        count += 1
+            if count == N:
+                count_finaly += N
+
+    q += 1
+    return count_finaly
+
+
+if __name__ == '__main__':
+    N, M, Q = map(int, input().split())
+    print(query(N, N, Q))
+
+
+
+
