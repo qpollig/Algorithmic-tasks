70af0c0116015e8c52b5e45ddad7ec9e56c11481
Polina Nemechkina <qpollig@gmail.com>
2023-12-01 21:39:30 +0500

    orting by merging blocks the decision has reached an impasse nex

--- sorting_by_merging_blocks.py
+++ sorting_by_merging_blocks.py
@@ -1,31 +1,30 @@
 import sys
 
 
-def find_blox(arr, zero_index, left_pointer, count):
-    r = len(arr[left_pointer:zero_index + 1])
-    if zero_index < 1:
-        return zero_index, count
-    elif r - 1 != max(*arr[left_pointer:zero_index]):
-        zero_index += 1
-        return find_blox(arr, zero_index, left_pointer, count)
-    else:
-        count += 1
-        zero_index += 1
-        return zero_index, count
+def counted(n, arr):
+    blocks = []
+    curr_block = 1
+    max_block = 1
 
+    for i in range(1, n - 1):
+        if (arr[i] > arr[i - 1]) and (arr[i] < arr[i + 1]):
+            curr_block += 1
+        else:
+            if curr_block > max_block:
+                max_block = curr_block
+            blocks.append(curr_block)
+            curr_block = 1
 
-def counter_blox(n: int, arr: list):
-    count = 0
-    zero_index = arr.index(0)
-    left_pointer = find_blox(arr, zero_index, 0, count)[0] + 1
-    while left_pointer < len(arr):
-        left_pointer, count = find_blox(arr, zero_index, left_pointer, count)
-    return count
+    if curr_block > max_block:
+        max_block = curr_block
+    blocks.append(curr_block)
+
+    return max_block + 1
 
 
 if __name__ == '__main__':
     n = int(sys.stdin.readline().rstrip())
-    arr = list(map(int, sys.stdin.readline().rstrip().split()))
-    print(counter_blox(n, arr))
+    numbers = list(map(int, sys.stdin.readline().rstrip().split()))
+    print(counted(n, numbers))
 
 
